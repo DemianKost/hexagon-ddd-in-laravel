eecbd86e1abd28d7b033c7a6cb59323f68cf8f52
Demian <demiankost2@gmail.con>
2025-08-28 16:39:12 -0700

    feature/tests: update workflow controller test

--- tests/Feature/WorkspaceControllerTest.php
+++ tests/Feature/WorkspaceControllerTest.php
@@ -2,29 +2,35 @@
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Src\Agenda\User\Infastructure\EloquentModels\UserEloquentModel;
+use Database\Factories\UserFactory;
 
 uses(RefreshDatabase::class);
 
-it('Create a new workspace', function() {
-    $response = $this->post('/user', [
-        'name' => 'test',
-        'email' => 'test@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password'
-    ]);
+describe('WorkspaceController main endpoints tests with auth', function() {
+    beforeEach(function () {
+        $this->user = UserFactory::new()->create();
+    });
 
-    $user = UserEloquentModel::find(1);
+    it('Create a new workspace', function() {
+        $response = $this->actingAs($this->user)
+            ->post('/workspace', [
+                'name' => 'Test workspace',
+                'is_active' => true,
+            ]);
+        
+        $response->assertStatus(200)
+            ->assertJson([
+                'name' => 'Test workspace',
+                'user_id' => 1,
+                'is_active' => true,
+            ]);
+    });
 
-    $response = $this->actingAs($user)
-        ->post('/workspace', [
-            'name' => 'Test workspace',
-            'is_active' => true,
-        ]);
-    
-    $response->assertStatus(200)
-        ->assertJson([
-            'name' => 'Test workspace',
-            'user_id' => 1,
-            'is_active' => true,
-        ]);
+    it('Can\'t create a new workspace with empty body', function() {
+        $response = $this->actingAs($this->user)
+            ->post('/workspace', [
+                'name' => 'Test workspace',
+                'is_active' => true,
+            ]);
+    });
 });
